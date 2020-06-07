9bb9ed381794ec26493d75ec3eedaae343ad636f
wangjiawei1996 <906789088@qq.com>
2020-06-08 00:26:50 +0800

    any type and combine type in ts how to use

--- hello.ts
+++ hello.ts
@@ -8,3 +8,10 @@ const age: number = 23;
 const binaryNumber: number = 0b111;
 const firstName: string = "Jiawei";
 let message: string = `hello, ${firstName}, age is ${age}`;
+
+let notSure: any = 4;
+
+notSure = "123";
+
+let numberorString: number | string = 123;
+numberorString = "23";
