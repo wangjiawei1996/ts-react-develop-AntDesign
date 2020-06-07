a3ef75458c91a55e7ddc72c0a5f7f95df3c9429c
wangjiawei1996 <906789088@qq.com>
2020-06-08 00:15:45 +0800

    first commit

---
+++ hello.ts
@@ -0,0 +1,10 @@
+const hello = (name: string) => {
+  return `hello &{name}`;
+};
+hello("Jiawei");
+
+const isDone: boolean = false;
+const age: number = 23;
+const binaryNumber: number = 0b111;
+const firstName: string = "Jiawei";
+let message: string = `hello, ${firstName}, age is ${age}`;
