3680fa47aee174e02b3915f08a9248729bbab662
wangjiawei1996 <906789088@qq.com>
2020-06-10 10:42:49 +0800

    class constructor function and create super extends father's sunction

---
+++ class.ts
@@ -0,0 +1,19 @@
+class Animal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  run() {
+    return `${this.name} is running`;
+  }
+}
+const animal = new Animal("Lili");
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  run() {
+    return "meol," + super.run();
+  }
+}
