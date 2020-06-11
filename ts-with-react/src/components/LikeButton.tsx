a1e631ae21ea6e7fa6d49dbb26dd40ba0134fddd
wangjiawei1996 <906789088@qq.com>
2020-06-11 10:55:34 +0800

    useEffect in ts and create void function

--- ts-with-react/src/components/LikeButton.tsx
+++ ts-with-react/src/components/LikeButton.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const LikeButtn: React.FC = () => {
   const [like, setLike] = useState(0);
   const [on, setOn] = useState(true);
+  useEffect(() => {
+    document.title = `点击了${like}次`;
+  });
   return (
     <div>
       <button
