9832f5b48ca5fa1882ce4c74585be6203641aad2
wangjiawei1996 <906789088@qq.com>
2020-06-30 16:27:53 +0800

    write component jest some boolean  number Object this function

---
+++ yuhang/src/jest.test.js
@@ -0,0 +1,19 @@
+test('test commmon matcher', () => {
+  expect(2 + 2).toBe(4)
+  expect(2 + 2).not.toBe(5)
+})
+test('test to be true or false', () => {
+  expect(1).toBeTruthy()
+  expect(0).toBeFalsy()
+})
+test(('test number'), () => {
+  expect(4).toBeGreaterThan(3)
+  expect(2).toBeLessThan(4)
+})
+test('test Object', () => {
+  expect({
+    name: 'Yuhang'
+  }).toEqual({
+    name: 'Yuhang'
+  })
+})
