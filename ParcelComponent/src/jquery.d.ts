9933ae848180ffecba3d17085a3eb76d76f9f437
wangjiawei1996 <906789088@qq.com>
2020-08-31 23:36:30 +0800

    declare namespace and create constructor new class

--- ParcelComponent/src/jquery.d.ts
+++ ParcelComponent/src/jquery.d.ts
@@ -2,7 +2,22 @@
 // declare var $: (param: () => void) => void;
 //全局函数
 interface JqueryInstance {
-  html: (html: string) => {};
+  html: (html: string) => JqueryInstance;
 }
+//函数重载
 declare function $(readyFunc: () => void): void;
 declare function $(selector: string): JqueryInstance;
+
+declare namespace $ {
+  namespace fn {
+    class init {}
+  }
+}
+
+///使用interface的语法，实现函数重载
+
+// interface Jquery {
+//   (readyFunc: () => void): void;
+//   (selector: string): JqueryInstance;
+// }
+// declare var $: Jquery;
