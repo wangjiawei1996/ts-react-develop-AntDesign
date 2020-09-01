087544b4267f100eb4f0a709fa18179070817b99
wangjiawei1996 <906789088@qq.com>
2020-09-01 23:42:55 +0800

    declare module in .d.ts and export this params using ts

--- ParcelComponent/src/jquery.d.ts
+++ ParcelComponent/src/jquery.d.ts
@@ -1,17 +1,18 @@
 //全局变量
 // declare var $: (param: () => void) => void;
 //全局函数
-interface JqueryInstance {
-  html: (html: string) => JqueryInstance;
-}
-//函数重载
-declare function $(readyFunc: () => void): void;
-declare function $(selector: string): JqueryInstance;
-
-declare namespace $ {
-  namespace fn {
-    class init {}
+declare module 'jquery' {
+  interface JqueryInstance {
+    html: (html: string) => JqueryInstance;
   }
+  function $(readyFunc: () => void): void;
+  function $(selector: string): JqueryInstance;
+  namespace $ {
+    namespace fn {
+      class init {}
+    }
+  }
+  export = $;
 }
 
 ///使用interface的语法，实现函数重载
