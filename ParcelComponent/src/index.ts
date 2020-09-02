1bb4a5a8b29526235be6e631ed76241b397e48a1
wangjiawei1996 <906789088@qq.com>
2020-09-02 22:24:36 +0800

    interface using keyof to sublime this object and selector this type

--- ParcelComponent/src/index.ts
+++ ParcelComponent/src/index.ts
@@ -1,5 +1,17 @@
-import $ from 'jquery';
-$(function () {
-  $('body').html('<div>123</div');
-  new $.fn.init();
+interface Person {
+  name: string;
+  age: number;
+  gener: string;
+}
+class Teacher {
+  constructor(private info: Person) {}
+  getInfo<T extends keyof Person>(key: T): Person[T] {
+    return this.info[key];
+  }
+}
+const teacher = new Teacher({
+  name: 'dell',
+  age: 30,
+  gener: 'male',
 });
+const test = teacher.getInfo('name') as string;
