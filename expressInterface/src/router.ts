acc7584a41341fe1b4435e3ac636cde391cde60a
wangjiawei1996 <906789088@qq.com>
2020-09-14 16:31:35 +0800

    controller some data using in ts and using getResponseData

--- expressInterface/src/router.ts
+++ expressInterface/src/router.ts
@@ -1,16 +1,25 @@
-import { Router, Request, Response } from 'express';
-import Crowller from './crowller';
-import DellAnalyzer from './dellAnalyzer';
+import { Router, Request, Response, NextFunction } from 'express';
+import Crowller from './utils/crowller';
+import Analyzer from './utils/analyzer';
 import fs from 'fs';
 import path from 'path';
+import { getResponseData } from './utils/util';
 
-interface RequestWithBody extends Request {
+interface BodyRequest extends Request {
   body: {
     [key: string]: string | undefined;
   };
 }
+const checkLogin = (req: BodyRequest, res: Response, next: NextFunction) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    next();
+  } else {
+    res.json(getResponseData(null, '请先登录'));
+  }
+};
 const router = Router();
-router.get('/', (req: Request, res: Response) => {
+router.get('/', (req: BodyRequest, res: Response) => {
   const isLogin = req.session ? req.session.login : false;
   if (isLogin) {
     res.send(`
@@ -35,50 +44,40 @@ router.get('/', (req: Request, res: Response) => {
   `);
   }
 });
-router.get('/logout', (req: Request, res: Response) => {
+router.get('/logout', (req: BodyRequest, res: Response) => {
   if (req.session) {
     req.session.login = undefined;
   }
-  res.redirect('/');
+  res.json(getResponseData(true));
 });
-router.post('/login', (req: RequestWithBody, res: Response) => {
+router.post('/login', (req: BodyRequest, res: Response) => {
   const { password } = req.body;
   const isLogin = req.session ? req.session.login : false;
   if (isLogin) {
-    res.send('已经登录过了');
+    res.json(getResponseData(false, '已经登录过了'));
   } else {
     if (password === '123' && req.session) {
       req.session.login = true;
-      res.send('登录成功');
+      res.json(getResponseData(true));
     } else {
-      res.send('登录失败');
+      res.json(getResponseData(false, '登录失败'));
     }
   }
 });
-router.get('/get', (req: RequestWithBody, res: Response) => {
-  const isLogin = req.session ? req.session.login : false;
-  if (isLogin) {
-    const secret = 'secretKey';
-    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-    const analyzer = DellAnalyzer.getInstance();
-    new Crowller(url, analyzer);
-    res.send('bye world');
-  } else {
-    res.send('password Error');
-  }
+router.get('/get', checkLogin, (req: BodyRequest, res: Response) => {
+  const secret = 'secretKey';
+  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+  const analyzer = Analyzer.getInstance();
+  new Crowller(url, analyzer);
+  res.json(getResponseData(true));
 });
-router.get('/showData', (req: RequestWithBody, res: Response) => {
-  const isLogin = req.session ? req.session.login : false;
-  if (isLogin) {
-    try {
-      const position = path.resolve(__filename, '../data/course.json');
-      const result = fs.readFileSync(position, 'utf-8');
-      res.json(JSON.parse(result));
-    } catch {
-      res.send('尚未爬取到数据');
-    }
-  } else {
-    res.send('请登录后查看内容');
+router.get('/showData', checkLogin, (req: BodyRequest, res: Response) => {
+  try {
+    const position = path.resolve(__filename, '../data/course.json');
+    const result = fs.readFileSync(position, 'utf-8');
+    res.json(JSON.parse(result));
+  } catch (e) {
+    res.json(getResponseData(false, '数据不存在'));
   }
 });
 export default router;
