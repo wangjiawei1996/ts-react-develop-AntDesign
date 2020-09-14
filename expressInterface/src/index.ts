f551e89b46db4fde5b3699c7b50e50351ea3f7e9
wangjiawei1996 <906789088@qq.com>
2020-09-14 15:10:11 +0800

    using cookieSession to save login some data and crowller express in page

--- expressInterface/src/index.ts
+++ expressInterface/src/index.ts
@@ -1,12 +1,16 @@
 import express, { Response, Request, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import router from './router';
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req: Request, res: Response, next: NextFunction) => {
-  req.teacherName = 'dell';
-  next();
-});
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['teacher dell'],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 app.use(router);
 app.listen(7001, () => {
   console.log('server is running');
--- expressInterface/src/router.ts
+++ expressInterface/src/router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import Crowller from './crowller';
 import DellAnalyzer from './dellAnalyzer';
+import fs from 'fs';
+import path from 'path';
 
 interface RequestWithBody extends Request {
   body: {
@@ -9,27 +11,74 @@ interface RequestWithBody extends Request {
 }
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
-  res.send(`
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send(`
+      <html>
+        <body>
+          <a href='/get'>爬取内容</a>
+          <a href='/showData'>展示内容</a>
+          <a href='/logout'>退出</a>
+        </body>
+      </html>
+    `);
+  } else {
+    res.send(`
     <html>
       <body>
-        <form method="post" action="/get">
+        <form method="post" action="/login">
           <input type="password" name="password" />
-          <button>提交</button>
+          <button>登录</button>
         </form>
       </body>
     </html>
   `);
+  }
 });
-router.post('/get', (req: RequestWithBody, res: Response) => {
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined;
+  }
+  res.redirect('/');
+});
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const { password } = req.body;
-  if (password === '123') {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    res.send('已经登录过了');
+  } else {
+    if (password === '123' && req.session) {
+      req.session.login = true;
+      res.send('登录成功');
+    } else {
+      res.send('登录失败');
+    }
+  }
+});
+router.get('/get', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
     const secret = 'secretKey';
     const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
     const analyzer = DellAnalyzer.getInstance();
     new Crowller(url, analyzer);
     res.send('bye world');
   } else {
-    res.send(`${req.teacherName}password Error`);
+    res.send('password Error');
+  }
+});
+router.get('/showData', (req: RequestWithBody, res: Response) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    try {
+      const position = path.resolve(__filename, '../data/course.json');
+      const result = fs.readFileSync(position, 'utf-8');
+      res.json(JSON.parse(result));
+    } catch {
+      res.send('尚未爬取到数据');
+    }
+  } else {
+    res.send('请登录后查看内容');
   }
 });
 export default router;
