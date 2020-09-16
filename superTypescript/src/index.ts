583cf152fcb722f415b73f7c9a2959f0bc8a9e43
wangjiawei1996 <906789088@qq.com>
2020-09-16 15:08:29 +0800

    using get set this function decorator style another

--- superTypescript/src/index.ts
+++ superTypescript/src/index.ts
@@ -4,15 +4,17 @@ function getNameDecorator(
   descriptor: PropertyDescriptor
 ) {}
 class Test {
-  name: string;
+  private _name: string;
   constructor(name: string) {
-    this.name = name;
+    this._name = name;
   }
-  @getNameDecorator
-  getName() {
-    return this.name;
+  get name() {
+    return this._name;
+  }
+  set name(name: string) {
+    this._name = name;
   }
 }
 
-const test = new Test('dell');
-console.log(test.name);
+// const test = new Test('dell');
+// console.log(test.name);
